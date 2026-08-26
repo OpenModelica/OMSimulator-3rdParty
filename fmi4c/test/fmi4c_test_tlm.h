ad64e2b5e6eca990b5d8f2d408cd8cad5d977bfd
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2026-08-26 16:16:21 +0200

    Update fmi4c to v1.2
    * Removed copy of fmi4c and added it as submodule

--- fmi4c/test/fmi4c_test_tlm.h
+++
@@ -1,8 +0,0 @@
-#ifndef FMIC_TEST_TLM_H
-#define FMIC_TEST_TLM_H
-
-#include "fmi4c.h"
-
-int testFMI3TLM(fmiHandle *fmua, fmiHandle *fmub, bool overrideStopTime, double stopTimeOverride, bool overrideTimeStep, double timeStepOverride);
-
-#endif //FMIC_TEST_TLM_H
